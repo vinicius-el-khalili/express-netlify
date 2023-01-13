865299068134fd1e4e8433fe2f493babc811a89b
vinicius <velkhaliliborsato@gmail.com>
2023-01-13 02:19:13 -0300

    (+) netlify.toml api.js

---
+++ src/api.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const serverless = require("serverless-http")
+const app = express()
+const router = express.Router()
+router.get("/",(req,res)=>{
+    res.json({"msg":"hello world!"})
+})
+app.use('./netlify/functions/api',router)
